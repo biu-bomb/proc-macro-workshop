17509fc2eafb2a1ffa1feb4310234fc236da512c
laofu <laofu@pinduoduo.com>
2022-06-13 13:29:11 +0800

    degub 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,11 @@
-use proc_macro::TokenStream;
-
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    solution1(input)
+}
 
-    unimplemented!()
+fn solution1(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _files = syn::parse_macro_input!(input as syn::DeriveInput);
+    let token_stream = proc_macro2::TokenStream::new();
+
+    proc_macro::TokenStream::from(token_stream)
 }
